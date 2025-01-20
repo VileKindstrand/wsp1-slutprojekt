8f2fcc2d1a20bb9b3d92da0f482d0ea048728e3c
VileKindstrand <vile.kindstrand@elev.ga.ntig.se>
2025-01-20 14:53:44 +0100

    första committen
    har lagt till lite glitter i:
    - seeder
    - index.rb
    - app

--- app.rb
+++ app.rb
@@ -1,7 +1,62 @@
 class App < Sinatra::Base
 
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todo.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
+
     get '/' do
+        redirect("/views")
+    end
+
+    get '/views' do
+        @todo_items = db.execute('SELECT * FROM todo ORDER BY price ASC')
+        @rate_items = db.execute('SELECT * FROM rate')
         erb(:"index")
     end
 
+
+
+    # get '/views/new' do
+    #     erb(:"/new")
+    #     redirect("/views")
+    # end
+
+    post '/views/new' do
+
+        name = params["item_name"]
+        description = params["item_description"]
+        category = params["item_category"]
+        price = params["item_price"]
+
+        db.execute("INSERT INTO todo (name, description, price) VALUES(?,?,?)", [name, description, price])
+        redirect("/views")
+    end
+
+    post '/views/:id/delete' do |id|
+        #hämtar id och deletar i databas "todo" där id matchar hämtade id
+        db.execute("DELETE FROM todo WHERE id=?", id)
+        redirect("/views")
+    end
+    
+    # post '/views/:id/rate' do |id|
+    #     #hämtar id och deletar i databas "todo" där id matchar hämtade id
+    #     db.execute("INSERT INTO rated WHERE id=?", id)
+    #     redirect("/views")
+    # end
+    
+
+    
+
+    
+    
+    
+
 end
+
+
+
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,7 +3,26 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    ####################item######################
+    db.execute('DROP TABLE IF EXISTS item;')
+    db.execute('CREATE TABLE IF NOT EXISTS item (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      description TEXT,
+      price FLOAT,
+      category TEXT)')
+    db.execute('INSERT INTO item (name, description, category, price) VALUES ("grunka", "grön och går att äta", "grönsak", 10)')
+    db.execute('INSERT INTO item (name, description, category, price) VALUES ("grön borr", "grön och kan borra", "grön sak",  20)')
+    db.execute('INSERT INTO item (name, description, category, price) VALUES ("buske", "grön och typ buskig", "grönska", 30)')
+    ##################rate#####################
+  end
+
+
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/item.sqlite')
+    @db.results_as_hash = true
+    @db
   end
 
 end
