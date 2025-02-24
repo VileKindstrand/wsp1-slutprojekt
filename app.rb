a71a7f2dcc6a4d8ac11bf6f8b6596fc31b571dea
VileKindstrand <vile.kindstrand@elev.ga.ntig.se>
2025-02-24 12:59:56 +0100

    oj

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ class App < Sinatra::Base
     def db
         return @db if @db
 
-        @db = SQLite3::Database.new("db/todo.sqlite")
+        @db = SQLite3::Database.new("db/item.sqlite")
         @db.results_as_hash = true
 
         return @db
@@ -14,8 +14,8 @@ class App < Sinatra::Base
     end
 
     get '/views' do
-        @todo_items = db.execute('SELECT * FROM todo ORDER BY price ASC')
-        @rate_items = db.execute('SELECT * FROM rate')
+        @item_items = db.execute('SELECT * FROM item ORDER BY price ASC')
+        @item_category = db.execute('SELECT DISTINCT category FROM item')
         erb(:"index")
     end
 
@@ -33,21 +33,43 @@ class App < Sinatra::Base
         category = params["item_category"]
         price = params["item_price"]
 
-        db.execute("INSERT INTO todo (name, description, price) VALUES(?,?,?)", [name, description, price])
+        db.execute("INSERT INTO item (name, description, category, price) VALUES(?,?,?,?)", [name, description, category, price])
         redirect("/views")
     end
 
     post '/views/:id/delete' do |id|
-        #hämtar id och deletar i databas "todo" där id matchar hämtade id
-        db.execute("DELETE FROM todo WHERE id=?", id)
+        #hämtar id och deletar i databas "item" där id matchar hämtade id
+        db.execute("DELETE FROM item WHERE id=?", id)
         redirect("/views")
     end
+
+    get '/item/:id' do |id|
+        #hämtar id och deletar i databas "item" där id matchar hämtade id
+        @item = db.execute('SELECT * FROM item WHERE id=?', id).first
+        erb(:"item")
+    end
+
+    get '/category/:category' do |category|
+        db.results_as_hash = true  # Se till att resultatet returneras som hash
+        @items_by_category = db.execute('SELECT * FROM item WHERE category = ?', category)
     
-    # post '/views/:id/rate' do |id|
-    #     #hämtar id och deletar i databas "todo" där id matchar hämtade id
-    #     db.execute("INSERT INTO rated WHERE id=?", id)
-    #     redirect("/views")
-    # end
+        # Om inga objekt hittas, skicka en 404-sida
+        halt 404, "Inga objekt hittades i denna kategori" if @items_by_category.empty?
+    
+        erb :category_list  # Kopplar till vyn
+    end
+    
+
+    get '/views/landing' do
+        @item_category = db.execute('SELECT category FROM item')
+        erb(:"landing")
+    end
+
+    get '/views/:id/select' do
+        @item_items = db.execute('SELECT * FROM item WHERE category=?', id)
+        erb(:"landing")
+    end
+
     
 
     
